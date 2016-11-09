346dfd2b69128fb7694d7c4ad2dfc3b349dc4faf
leegao <lg342@cornell.edu>
2016-11-08 16:02:34 -0800

    Add driver for yuck

---
+++ src/com/yuck/Yuckc.java
@@ -0,0 +1,31 @@
+package com.yuck;
+
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Yuckc {
+  @Argument
+  private List<String> arguments = new ArrayList<>();
+
+  public static void main(String[] args) {
+    new Yuckc().driver(args);
+  }
+
+  public void driver(String[] args) {
+    CmdLineParser parser = new CmdLineParser(this);
+    try {
+      parser.parseArgument(args);
+    } catch (CmdLineException e) {
+      System.err.println(e.getMessage());
+      parser.printUsage(System.err);
+      System.err.println();
+      return;
+    }
+
+    System.out.println(arguments);
+  }
+}
