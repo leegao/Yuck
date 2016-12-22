b5929e5f3d1cd644b5823b226315163eb3f696e7
Lee Gao <lg342@cornell.edu>
2016-12-21 22:04:34 -0600

    Add compilation semantics for assignments

--- java/com/yuck/ast/AssignmentStatement.java
+++ java/com/yuck/ast/AssignmentStatement.java
@@ -1,6 +1,8 @@
 package com.yuck.ast;
 
 import com.yuck.grammar.Token;
+import com.yuck.ycode.Opcode;
+import com.yuck.ycode.YCodeFunctionContext;
 
 public class AssignmentStatement extends Statement {
   public final String id;
@@ -10,5 +12,10 @@ public class AssignmentStatement extends Statement {
     super(id.startLine, id.startColumn, semi.endLine, semi.endColumn);
     this.id = id.text;
     this.expr = expr;
+  }
+
+  @Override
+  public YCodeFunctionContext compile(YCodeFunctionContext context) {
+    return expr.compile(context).emit(Opcode.STORE_LOCAL, id);
   }
 }
