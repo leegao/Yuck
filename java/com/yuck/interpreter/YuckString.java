4a63d722a3029410b76cb083691664b83f8350f7
Lee Gao <lg342@cornell.edu>
2016-12-31 12:08:16 -0600

    Add some string lib

--- java/com/yuck/interpreter/YuckString.java
+++ java/com/yuck/interpreter/YuckString.java
@@ -1,13 +1,17 @@
 package com.yuck.interpreter;
 
+import com.yuck.interpreter.builtins.NativeModule;
+import sun.reflect.generics.reflectiveObjects.NotImplementedException;
+
 import java.util.Objects;
 
-public class YuckString extends YuckObject {
+public class YuckString extends YuckModule implements NativeModule {
   public final String string;
 
   public YuckString(String string, InterpreterContext context) {
     super(context);
     this.string = string;
+    registerAll();
   }
 
   @Override
@@ -33,9 +37,35 @@ public class YuckString extends YuckObject {
     return string.length() != 0;
   }
 
-
   @Override
   public String toString() {
     return string;
+  }
+
+  public void registerAll() {
+    register("length", c -> wrap(string.length()), context);
+    register("replace", c -> wrap(string.replace(unwrap(c.get(0)), unwrap(c.get(1)))), context);
+  }
+
+  private YuckObject wrap(Object object) {
+    if (object instanceof Integer) {
+      return new YuckInteger((Integer) object, context);
+    } else if (object instanceof Float) {
+      return new YuckFloat((Float) object, context);
+    } else if (object instanceof Double) {
+      return new YuckFloat(((Double) object).floatValue(), context);
+    } else if (object instanceof String) {
+      return new YuckString((String) object, context);
+    } else {
+      throw new NotImplementedException();
+    }
+  }
+
+  private <T> T unwrap(YuckObject yuckObject) {
+    if (yuckObject instanceof YuckString) {
+      return (T) ((YuckString) yuckObject).string;
+    } else {
+      throw new NotImplementedException();
+    }
   }
 }
